fcfb0e5643b7b6e802f9814b004a718c01473155
Rutuja15883 <rutujasaipawar15@gmail.com>
2023-02-17 23:47:42 +0530

    datatype
    datatype

---
+++ 02_datatypes.js
@@ -0,0 +1,9 @@
+var myName="Rutuja";
+var age=39;
+var weight=69.4;
+
+var isMarried=true;
+var isLazy=false;
+var typeOfMyName=typeof myName;
+console.log("To check the variable typeof",typeOfMyName);
+console.log("check datatype",typeof age);
